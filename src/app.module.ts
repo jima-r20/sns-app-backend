ddcea5e40a65a43a1e049bc396de183dbf07935a
ryuji <ryu.seven.h7420@gmail.com>
2020-10-27 21:02:42 +0900

    set up follow module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+import { FollowModule } from './follow/follow.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), UserModule, FollowModule],
 })
 export class AppModule {}
