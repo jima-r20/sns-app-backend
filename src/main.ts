c5c00c194a9e17fc203461ece34ee33ab0813419
ryuji <ryu.seven.h7420@gmail.com>
2020-11-07 15:16:21 +0900

    CORS設定

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,20 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // const serverConfig = config.get('server')
+
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors(
+    //   {
+    //   origin: '*',
+    //   allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept'
+    // }
+    );
+  } 
+  // else {
+  //   app.enableCors({ origin: serverConfig.origin });
+  // }
+
   await app.listen(3000);
 }
 bootstrap();
